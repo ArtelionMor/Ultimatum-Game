af1964af9870cb58ccd9b7171b5d9d10861a2b54
Teliau <122630463+ArtelionMor@users.noreply.github.com>
2026-06-29 21:23:06 +0200

    Launch . bat

---
+++ Ultimatum Game/Launch Ultimatum Game.bat
@@ -0,0 +1,12 @@
+@echo off
+:: 1. Aller dans le dossier du jeu (dossier du .bat lui-meme, portable)
+cd /d "%~dp0"
+
+:: 2. Lancer le serveur Python dans une nouvelle fenêtre PowerShell
+start powershell -NoExit -Command "python -m http.server 8777 --directory web"
+
+:: 3. Attendre 2 secondes pour laisser le temps au serveur de bien démarrer
+timeout /t 2 /nobreak > NUL
+
+:: 4. Ouvrir le lien automatiquement dans ton navigateur par défaut
+start http://localhost:8777
